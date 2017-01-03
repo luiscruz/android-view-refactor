1d88e70fb4dc1c8b305a8634a1ffd61b98aa53da
Luis Cruz <ei06034@fe.up.pt>
2017-01-03 16:09:16 +0000

    added batch file processing

--- android_view_refactor/android_view_refactor.py
+++ android_view_refactor/android_view_refactor.py
@@ -1,5 +1,6 @@
 import click
 from lxml import etree
+from lxml.etree import XMLSyntaxError
 from android_constants import ANDROID_VIEW_GROUPS  # supported ViewGroups
 
 INVALID_PARAMS = {
@@ -67,70 +68,82 @@ NAMESPACE = "{http://schemas.android.com/apk/res/android}"
     '--comment', default=False, is_flag=True,
     help='Leave a comment for each refactoring.'
 )
-@click.argument('input')
-def tool(refactor, comment, input):
+@click.argument('files', type=click.Path(exists=True), nargs=-1)
+def tool(refactor, comment, files):
     """Tool to refactor Android XML views."""
     parser = etree.XMLParser(
-       remove_blank_text=True
+        remove_blank_text=True
     )
-    tree = etree.parse(input, parser)
-    tree_has_changed = False
-    for node in tree.iter():
-        for attr_name, _ in node.attrib.items():
-            attr_simplename = attr_name.replace(NAMESPACE, "")
-            mandatory_parent_view_groups = INVALID_PARAMS.get(attr_simplename)
-            if mandatory_parent_view_groups is not None:
-                parent_view_group = node.getparent()
-                if parent_view_group is not None\
-                    and parent_view_group.tag in ANDROID_VIEW_GROUPS\
-                        and parent_view_group.tag\
-                        not in mandatory_parent_view_groups:
-                    error_msg = "{}:{} Warning: Invalid layout param in\
-                     a {}: {} [ObsoleteLayoutParam]".format(
-                        input,
-                        node.sourceline,
-                        parent_view_group.tag,
-                        attr_simplename
-                    )
-                    click.secho(
-                        error_msg,
-                        err=True,
-                        fg="red",
-                    )
-                    if refactor:
-                        if comment is not None:
-                            comment = etree.Comment(
-                                "Removed ObsoleteLayoutParam: {}"
-                                .format(attr_simplename)
-                            )
-                            node.append(comment)
-                        node.attrib.pop(attr_name)
-                        tree_has_changed = True
-    if refactor and tree_has_changed:
-        output = etree.tostring(
-            tree,
-            xml_declaration=True,
-            pretty_print=True,
-            encoding=tree.docinfo.encoding,
-        )
-        output = output.replace("\" ", "\"\n")
-        with open(input+"new", "w") as f:
-            parent_tag_line = None
-            parent_line_ident = None
-            for i, line in enumerate(output.splitlines()):
-                line_stripped = line.lstrip(" ")
-                line_ident = len(line) - len(line_stripped)
-                if parent_tag_line is not None:
-                    if line_ident == 0 and line[:2] != "</":
-                        line = (parent_line_ident + 2)*" " + line
+    for input in files:
+        try:
+            tree = etree.parse(input, parser)
+        except XMLSyntaxError:
+            click.secho(
+                "Could not parse {}".format(input),
+                err=True,
+                fg="red",
+            )
+            continue
+        tree_has_changed = False
+        for node in tree.iter():
+            for attr_name, _ in node.attrib.items():
+                attr_simplename = attr_name.replace(NAMESPACE, "")
+                mandatory_parent_view_groups = INVALID_PARAMS.get(
+                    attr_simplename)
+                if mandatory_parent_view_groups is not None:
+                    parent_view_group = node.getparent()
+                    if parent_view_group is not None\
+                        and parent_view_group.tag in ANDROID_VIEW_GROUPS\
+                            and parent_view_group.tag\
+                            not in mandatory_parent_view_groups:
+                        error_msg = (
+                            "{}:{} Warning: Invalid layout param in"
+                            " a {}: {} [ObsoleteLayoutParam]"
+                        ).format(
+                            input,
+                            node.sourceline,
+                            parent_view_group.tag,
+                            attr_simplename
+                        )
+                        click.secho(
+                            error_msg,
+                            err=True,
+                            fg="red",
+                        )
+                        if refactor:
+                            if comment is not None:
+                                comment = etree.Comment(
+                                    "Removed ObsoleteLayoutParam: {}"
+                                    .format(attr_simplename)
+                                )
+                                node.append(comment)
+                            node.attrib.pop(attr_name)
+                            tree_has_changed = True
+        if refactor and tree_has_changed:
+            output = etree.tostring(
+                tree,
+                xml_declaration=True,
+                pretty_print=True,
+                encoding=tree.docinfo.encoding,
+            )
+            output = output.replace("\" ", "\"\n")
+            with open(input + "new", "w") as f:
+                parent_tag_line = None
+                parent_line_ident = None
+                for i, line in enumerate(output.splitlines()):
+                    line_stripped = line.lstrip(" ")
+                    line_ident = len(line) - len(line_stripped)
+                    if parent_tag_line is not None:
+                        if line_ident == 0 and line[:2] != "</":
+                            line = (parent_line_ident + 2) * " " + line
+                        else:
+                            parent_tag_line = line
+                            parent_line_ident = line_ident
+                            line_stripped = line.lstrip()
+                            if line_stripped[:4] != "<!--"\
+                                    and line_stripped[:2] != "</":
+                                line = "\n" + line
                     else:
                         parent_tag_line = line
                         parent_line_ident = line_ident
-                        line_stripped = line.lstrip()
-                        if line_stripped[:4] != "<!--"\
-                                and line_stripped[:2] != "</":
-                            line = "\n" + line
-                else:
-                    parent_tag_line = line
-                    parent_line_ident = line_ident
-                print >>f, line
+                    print >>f, line

d4b6622d7f99edf178cc3e4b124b8538f3bd5cf9
Luis Cruz <ei06034@fe.up.pt>
2017-01-03 14:47:53 +0000

    added pbr

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
 setup(
-    name='android-view-refactor',
-    version='0.1',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'Click',
-        'lxml',
+    setup_requires=[
+        'pbr>=1.9',
     ],
-    entry_points='''
-        [console_scripts]
-        android-view-refactor=android_view_refactor.android_view_refactor:tool
-    ''',
+    pbr=True,
 )

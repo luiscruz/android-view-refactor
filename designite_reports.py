78dbbd13a929f841d79f6bf8f2a41f327793b291
Luis Cruz <ei06034@fe.up.pt>
2019-01-25 12:25:28 +0000

    designite scripts

--- designite_reports.py
+++ designite_reports.py
@@ -3,15 +3,18 @@ import pandas as pd
 
 path = "/Users/luiscruz/dev/designite/output"
 
+
 def _read_multiple_csv(path):
     all_files = glob.glob(path)
     list_ = []
     for file_ in all_files:
-        df = pd.read_csv(file_,index_col=None, header=0)
+        df = pd.read_csv(file_, index_col=None, header=0)
         list_.append(df)
-    return pd.concat(list_, axis = 0, ignore_index = True)
+    return pd.concat(list_, axis=0, ignore_index=True)
+
 
 type_metrics_frame = _read_multiple_csv(path + "/*/typeMetrics.csv")
 method_metrics_frame = _read_multiple_csv(path + "/*/methodMetrics.csv")
 
-import pdb; pdb.set_trace()
+type_metrics_frame.to_csv("AllTypeMetrics.csv")
+method_metrics_frame.to_csv("AllMethodMetrics.csv")
